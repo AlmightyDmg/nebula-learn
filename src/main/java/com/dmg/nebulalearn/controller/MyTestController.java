1682bca136cbd1a40a20df3f1233afd26d949035
zhumeng <zhumeng@haizhi.com>
2023-10-18 16:44:31 +0800

    集成ngbatis前

--- src/main/java/com/dmg/nebulalearn/controller/MyTestController.java
+++ src/main/java/com/dmg/nebulalearn/controller/MyTestController.java
@@ -2,9 +2,13 @@ package com.dmg.nebulalearn.controller;
 
 import javax.annotation.Resource;
 
-import com.dmg.nebulalearn.service.PersonServiceImpl;
+import com.dmg.nebulalearn.model.vertex.Country;
+import com.dmg.nebulalearn.service.MyTestServiceImpl;
+import io.github.anyzm.graph.ocean.domain.impl.QueryResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyTestController {
 
     @Resource
-    private PersonServiceImpl personService;
+    private MyTestServiceImpl myTestService;
 
     @GetMapping("/insertVertexAndEdge")
     public void insertVertexAndEdge() {
-        personService.insertVertexAndEdge();
+        myTestService.insertVertexAndEdge();
+    }
+
+    @PostMapping("/insertCountryVertex")
+    public boolean insertCountryVertex(@RequestBody Country country) {
+        return myTestService.insertCountryVertex(country);
+    }
+
+    @GetMapping("/getAllCountry")
+    public QueryResult getAllCountry(){
+        return myTestService.getAllCountry();
     }
 }
--- src/main/java/com/dmg/nebulalearn/service/MyTestServiceImpl.java
+++ src/main/java/com/dmg/nebulalearn/service/MyTestServiceImpl.java
@@ -1,26 +1,27 @@
 package com.dmg.nebulalearn.service;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import com.dmg.nebulalearn.model.edge.OrgToOrg;
-import com.dmg.nebulalearn.model.edge.Organiza;
 import com.dmg.nebulalearn.model.vertex.Country;
 import com.dmg.nebulalearn.model.vertex.Organization;
-import com.dmg.nebulalearn.model.vertex.Person;
 import com.google.common.collect.Lists;
-import com.vesoft.nebula.client.graph.NebulaPoolConfig;
 import com.vesoft.nebula.client.graph.exception.AuthFailedException;
 import com.vesoft.nebula.client.graph.exception.ClientServerIncompatibleException;
 import com.vesoft.nebula.client.graph.exception.IOErrorException;
 import com.vesoft.nebula.client.graph.exception.NotValidConnectionException;
-import com.vesoft.nebula.client.graph.net.NebulaPool;
+import io.github.anyzm.graph.ocean.domain.GraphQuery;
+import io.github.anyzm.graph.ocean.domain.VertexQuery;
+import io.github.anyzm.graph.ocean.domain.impl.QueryResult;
+import io.github.anyzm.graph.ocean.engine.NebulaQueryUtils;
+import io.github.anyzm.graph.ocean.engine.NebulaVertexQuery;
 import io.github.anyzm.graph.ocean.mapper.NebulaGraphMapper;
-import io.github.anyzm.graph.ocean.session.NebulaPoolSessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PersonServiceImpl {
+public class MyTestServiceImpl {
 
     @Autowired
     private NebulaGraphMapper nebulaGraphMapper;
@@ -60,49 +61,54 @@ public class PersonServiceImpl {
             throw new RuntimeException(e);
         }
     }
-
-    public void demos() {
-        // 实现 两个节点插入
-//        Person tom = new Person();
-//        tom.setName("Tom");
-//
-//        dao.insert(tom);
-//
-//        Person jerry = new Person();
-//        jerry.setName( "Jerry" );
-//        dao.insert(jerry);
-//
-//        // 建立两个节点的关系
-//        Like like = new Like( 0.99999 );
-//        dao.insertEdge( tom, like, jerry );
-//
-//        // 查找喜欢 jerry 的人
-//        String jerryId = jerry.getName();
-//        List<Person> whoLikeJerry = dao.listStartNodes( Like.class, jerryId );
-//
-//        // 查找唯一喜欢 jerry 的人。非唯一时报错。（限定在特定关系仅有一个上游的场景）
-//        Person tom = dao.startNode( Like.class, jerryId );
-//
-//        // 查看 Tom 跟 Jerry 之间的 Like关系
-//        String tomId = tom.getName();
-//        Boolean tomLikeJerry = dao.existsEdge( tomId, Like.class, jerryId ); // true
-//        Boolean jerryLikeTom = dao.existsEdge( jerryId, Like.class, tomId ); // false
-//        // 可怜的 Tom
-//
-//        // 根据 Tom 的名字查找全部信息
-//        Person tomDb = dao.selectById( "Tom" );
-//
-//        // 查找分页
-//        Page<Person> page = new Page<>();
-//        List<Person> personPage = dao.selectPage( page );
-//        page.getTotal(); // 2 rows， Tom and Jerry
-//        Boolean theyAreFamily = page.getRows() == personPage; // true
-//
-//        // 故事总想要有个好的结局
-//        dao.insertEdge( jerry, like, tom );
-
-        // 更多 基类的操作还在开发中。期待
+    /**
+     * 新增国家
+     * @author zhum
+     * @date 2023/10/18 15:40
+     * @param country
+     * @return boolean
+     */
+    public boolean insertCountryVertex(Country country) {
+        try {
+            nebulaGraphMapper.saveVertexEntities(Collections.singletonList(country));
+        } catch (ClientServerIncompatibleException e) {
+            throw new RuntimeException(e);
+        } catch (AuthFailedException e) {
+            throw new RuntimeException(e);
+        } catch (NotValidConnectionException e) {
+            throw new RuntimeException(e);
+        } catch (IOErrorException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
     }
 
+    /**
+     * 获取所有的国家
+     * @author zhum
+     * @date 2023/10/18 15:59
+     * @param
+     * @return java.lang.String
+     */
+    public QueryResult getAllCountry() {
+        QueryResult records;
+        try {
+            VertexQuery vertexQuery = NebulaVertexQuery.build()
+                    .fetchPropOn(Country.class,"CN")
+                    .yield(Country.class,"code","name");
+            records = nebulaGraphMapper.executeQuery(vertexQuery);
 
+            //records = nebulaGraphMapper.executeQuerySql("match (v:country) return v");
+        } catch (ClientServerIncompatibleException e) {
+            throw new RuntimeException(e);
+        } catch (AuthFailedException e) {
+            throw new RuntimeException(e);
+        } catch (NotValidConnectionException e) {
+            throw new RuntimeException(e);
+        } catch (IOErrorException e) {
+            throw new RuntimeException(e);
+        }
+
+        return records;
+    }
 }
